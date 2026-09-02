70206828040dd1fa027aab59b81c58ea6b7c84a1
Adam Fuller <fullera@ohio.edu>
2026-09-02 19:11:00 +0000

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Subsection",
   "number": "",
   "title": "Course Information",
-  "body": " Course Information     Instructor  Adam Fuller, Morton 561, fullera@ohio.edu .   Teaching Assistant  Michael Shobe, Morton 239, ms712025@ohio.edu .    Student Hours  TBD     Course Description  First course in calculus and analytic geometry with applications in the sciences and engineering. Includes basic techniques of differentiation and integration with applications including rates of change, optimization problems, and curve sketching; includes exponential, logarithmic and trigonometric functions. Calculus is the mathematical language used to describe and analyze change. The course emphasizes how this abstract language and its associated techniques provide a unified way of approaching problems originating in disparate areas of science, technology, and society, highlighting how questions arising in different fields are connected to the same fundamental mathematical ideas. No credit for both MATH 2301 and 1350 (always keep 2301).    Prerequisite  (B or better in MATH 1350) or (C or better in 1300 or 1322) or (Math placement level 3)    Textbook and course materials   Essential Calculus: Early Transcendentals, by James Stewart, 2nd Edition ISBN-13: 9781133112280 (available through inclusive access).    Course Outcomes    Students will be able to use the tools of differential and integral calculus in a variety of applications.  Students will be able to explain information presented in mathematical forms (e.g., equations, graphs, diagrams, tables, words).  Students will be able to convert relevant information into various mathematical forms (e.g., equations, graphs, diagrams, tables, words).  Students will be able to calculate relevant information using various mathematical formulas.  Students will be able to make judgments and draw appropriate conclusions based on the quantitative analysis of data while recognizing the limits of this analysis.  Students will be able to make and evaluate important assumptions in estimation, modeling, and data analysis.  Students will be able to express quantitative evidence in support of the argument or purpose of the work (in terms of what evidence is used and how it is formatted, presented, and contextualized).  Students will be able to critically state, describe, and consider an issue or problem.  Students will be able to use information from source(s) with enough interpretation\/evaluation to develop a comprehensive analysis or synthesis.  Students will be able to systematically and methodically analyze assumptions and carefully evaluate the relevance of contexts when presenting a position.  Students will be able to state a specific position (i.e., perspective, thesis, or hypothesis) that is thoughtful, recognizes complexities, and acknowledges limitations.  Students will be able to state conclusions and related outcomes (consequences and implications) logically and in a priority order.       "
+  "body": " Course Information     Instructor  Adam Fuller, Morton 561, fullera@ohio.edu .    Doctor of Philosophy (Pure Mathematics), University of Waterloo (2012)  Master of Advanced Studies (Mathematics), University of Cambridge (2008)  Bachelor of Arts (Mathematics), Trinity College Dublin (2007)     Teaching Assistant  Michael Shobe, Morton 239, ms712025@ohio.edu .    Student Hours  TBD     Course Description  First course in calculus and analytic geometry with applications in the sciences and engineering. Includes basic techniques of differentiation and integration with applications including rates of change, optimization problems, and curve sketching; includes exponential, logarithmic and trigonometric functions. Calculus is the mathematical language used to describe and analyze change. The course emphasizes how this abstract language and its associated techniques provide a unified way of approaching problems originating in disparate areas of science, technology, and society, highlighting how questions arising in different fields are connected to the same fundamental mathematical ideas. No credit for both MATH 2301 and 1350 (always keep 2301).    Prerequisite  (B or better in MATH 1350) or (C or better in 1300 or 1322) or (Math placement level 3)    Textbook and course materials   Essential Calculus: Early Transcendentals, by James Stewart, 2nd Edition ISBN-13: 9781133112280 (available through inclusive access).    Course Outcomes    Students will be able to use the tools of differential and integral calculus in a variety of applications.  Students will be able to explain information presented in mathematical forms (e.g., equations, graphs, diagrams, tables, words).  Students will be able to convert relevant information into various mathematical forms (e.g., equations, graphs, diagrams, tables, words).  Students will be able to calculate relevant information using various mathematical formulas.  Students will be able to make judgments and draw appropriate conclusions based on the quantitative analysis of data while recognizing the limits of this analysis.  Students will be able to make and evaluate important assumptions in estimation, modeling, and data analysis.  Students will be able to express quantitative evidence in support of the argument or purpose of the work (in terms of what evidence is used and how it is formatted, presented, and contextualized).  Students will be able to critically state, describe, and consider an issue or problem.  Students will be able to use information from source(s) with enough interpretation\/evaluation to develop a comprehensive analysis or synthesis.  Students will be able to systematically and methodically analyze assumptions and carefully evaluate the relevance of contexts when presenting a position.  Students will be able to state a specific position (i.e., perspective, thesis, or hypothesis) that is thoughtful, recognizes complexities, and acknowledges limitations.  Students will be able to state conclusions and related outcomes (consequences and implications) logically and in a priority order.       "
 },
 {
   "id": "sec-assessments",
@@ -37,6 +37,15 @@ var ptx_lunr_docs = [
   "body": "Schedule    Week  In class work    8\/24 – 8\/28 M: Diagnostics    8\/31 - 9\/4  W: Quiz    9\/7 - 9\/11 (Labor Day 9\/7)  F: Quiz    9\/14 - 9\/18 F: Exam    9\/21 - 9\/25    9\/28 – 10\/2 (Fall Break 10\/2) M: Quiz    10\/5 – 10\/9 F: Quiz    10\/12 - 10\/16 W: Exam    10\/19 - 10\/23 F: Quiz    10\/26 - 10\/30 F: Quiz    11\/2 - 11\/6 F: Quiz    11\/9 - 11\/13 (Veteran’s Day 11\/11) F: Exam    11\/16 - 11\/19 F: Quiz    11\/23 - 11\/27 (Thanksgiving 11\/26 – 11\/28)    11\/30 - 12\/4 W: Quiz    12\/7 - 12\/11 (Exam week) Th: Final Exam (4:30- 6:40)     "
 },
 {
+  "id": "syllabus-6",
+  "level": "1",
+  "url": "syllabus-6.html",
+  "type": "Subsection",
+  "number": "",
+  "title": "Religious Accommodations",
+  "body": "Religious Accommodations  Per University Policy 40.003, Ohio University reasonably accommodates the sincerely held religious or spiritual beliefs and practices of students. You may be absent for up to three days each academic semester to take holidays for reasons of faith or religious or spiritual belief system or participate in organized activities conducted under the auspices of a religious denomination, church, or other religious or spiritual organization. You must request any accommodation in writing no later than 14 days after the first day of instruction.  There will be no academic penalty as a result of you being absent for sincerely held religious or spiritual beliefs. If you miss an assignment, quiz, or exam, you are expected to coordinate with the instructor to schedule an alternative. The alternative may be before or after the absence.  If you have questions about this policy, you may contact Civil Rights Compliance.   "
+},
+{
   "id": "ch--2",
   "level": "1",
   "url": "ch--2.html",
@@ -53,6 +62,60 @@ var ptx_lunr_docs = [
   "number": "1",
   "title": "",
   "body": "  Fill in the answers for the limit, left limit, right limit, and value for the function for .   Graph of   Plot of a piecewise defined function.       "
+},
+{
+  "id": "ch--3",
+  "level": "1",
+  "url": "ch--3.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Recitation 2",
+  "body": " Recitation 2    Instructions: fill in the appropriate answers in the spaces below.   Clearly write first and last names of each member on the top of the pages.      We will use the Squeeze Theorem to show      Find functions and such that          To apply the Squeeze Theorem we need Calculate (If go back and choose different and .)  Put your work for calculating and in the space below.      Apply the Squeeze Theorem to calculate          In this exercise you will use the Intermediate Value Theorem to study the roots of polynomials.     Use the Intermediate Value Theorem to show that the function has a root. That is, show that there is a solution to . (You do not have to find the root).      Let be any polynomial of degree . Explain why must have at least one root.      Let be any polynomial of odd degree. Explain why must have at least one root.      Does the same argument work for polynomials of even degree?      "
+},
+{
+  "id": "ch--3-3-1",
+  "level": "2",
+  "url": "ch--3.html#ch--3-3-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  We will use the Squeeze Theorem to show      Find functions and such that          To apply the Squeeze Theorem we need Calculate (If go back and choose different and .)  Put your work for calculating and in the space below.      Apply the Squeeze Theorem to calculate     "
+},
+{
+  "id": "ch--3-4-1",
+  "level": "2",
+  "url": "ch--3.html#ch--3-4-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  In this exercise you will use the Intermediate Value Theorem to study the roots of polynomials.     Use the Intermediate Value Theorem to show that the function has a root. That is, show that there is a solution to . (You do not have to find the root).      Let be any polynomial of degree . Explain why must have at least one root.      Let be any polynomial of odd degree. Explain why must have at least one root.      Does the same argument work for polynomials of even degree?    "
+},
+{
+  "id": "root-1-2-5-2",
+  "level": "1",
+  "url": "root-1-2-5-2.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Quiz 1",
+  "body": " Quiz 1    Answer all questions. Show your work. .  Clearly write your name on the top of the page.      Below is the graph of .    Plot of a piecewise defined function.         Fill in the answers based on the graph.            List the points where is not continuous. Which of these points are jump discontinuities ? Which of these points are removable discontinuities ?    The function is not continuous at , , and . Jump discontinuities at and . Removable discontinuity at          Evaluate the following limits. If the limit does not exist, write DNE.                      Note that we can't plug in the value , since that would give in the denominator. Instead, note that when , Hence        .    Here we will use the fact that . Note that Hence        .    Note that, for  Multiplying across by we get Since it follows from the Squeeze Theorem that       "
+},
+{
+  "id": "root-1-2-5-2-3-1",
+  "level": "2",
+  "url": "root-1-2-5-2.html#root-1-2-5-2-3-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Below is the graph of .    Plot of a piecewise defined function.         Fill in the answers based on the graph.            List the points where is not continuous. Which of these points are jump discontinuities ? Which of these points are removable discontinuities ?    The function is not continuous at , , and . Jump discontinuities at and . Removable discontinuity at     "
+},
+{
+  "id": "root-1-2-5-2-4-1",
+  "level": "2",
+  "url": "root-1-2-5-2.html#root-1-2-5-2-4-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Evaluate the following limits. If the limit does not exist, write DNE.                      Note that we can't plug in the value , since that would give in the denominator. Instead, note that when , Hence        .    Here we will use the fact that . Note that Hence        .    Note that, for  Multiplying across by we get Since it follows from the Squeeze Theorem that     "
 }
 ]
 
